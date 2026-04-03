ccc554c3c7400c5c1861a9878140994f9bbdc0da
Pritam Mandal <mandalpritam8617@gmail.com>
2026-04-04 01:22:07 +0530

    drizzle config file in JS

--- drizzle.config.js
+++ drizzle.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   schema: './src/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL,
     // url: "postgresql://postgres:postgres@localhost:5432/chaicode",
   },
 });
